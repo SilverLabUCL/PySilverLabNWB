54dfb3c12dae2fdb268fe1585d661094565cd64b
alessandrofelder <a.felder@ucl.ac.uk>
2020-04-17 11:43:31 +0100

    add synthetic new header to test data

--- tests/test_labview231_header.py
+++ tests/test_labview231_header.py
@@ -8,8 +8,8 @@ from silverlabnwb.header import LabViewHeader231, LabViewVersions, Modes
 DATA_PATH = os.environ.get('SILVERLAB_DATA_DIR', '')
 
 
-def do_header231_test(lab_view_folder, expected):
-    header = LabViewHeader231.from_file(DATA_PATH+lab_view_folder+"\\Experiment Header.ini")
+def do_header231_test(lab_view_header, expected):
+    header = LabViewHeader231.from_file(lab_view_header)
     trial_times = header.determine_trial_times()
     assert header.version == expected['version']
     assert len(trial_times) == expected['number of trials']
@@ -21,10 +21,14 @@ def do_header231_test(lab_view_folder, expected):
     reason="raw data folder '{}' not present".format(DATA_PATH))
 class TestLabView231Header(object):
 
+    def test_synthetic_header(self):
+        expected = {'version': LabViewVersions.v231, 'number of trials': 2, 'mode': Modes.miniscan}
+        do_header231_test(".\\tests\\data\\Experiment Header v231.ini", expected)
+
     def test_hg_30_exp01(self):
         expected = {'version': LabViewVersions.v231, 'number of trials': 30, 'mode': Modes.miniscan}
-        do_header231_test("\\LabViewData2020\\HG_30_exp01\\200107_13_13_33 FunctAcq", expected)
+        do_header231_test(DATA_PATH + "\\LabViewData2020\\HG_30_exp01\\200107_13_13_33 FunctAcq\\Experiment Header.ini", expected)
 
     def test_hg_29_exp02(self):
         expected = {'version': LabViewVersions.v231, 'number of trials': 30, 'mode': Modes.miniscan}
-        do_header231_test("\\LabViewData2020\\HG_29_exp02\\200206_16_30_32 FunctAcq", expected)
+        do_header231_test(DATA_PATH + "\\LabViewData2020\\HG_29_exp02\\200206_16_30_32 FunctAcq\\Experiment Header.ini", expected)
