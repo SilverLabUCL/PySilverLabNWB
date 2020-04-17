332830008d57de304a8b73042dfbf8fdd099f9b5
alessandrofelder <a.felder@ucl.ac.uk>
2020-04-17 12:03:06 +0100

    remove unnecessary header tests
    the new header tests now run on a synthetic .ini file

--- tests/test_labview231_header.py
+++ tests/test_labview231_header.py
@@ -1,11 +1,6 @@
 import os
 
-import pytest
-
 from silverlabnwb.header import LabViewHeader231, LabViewVersions, Modes
-
-# Where to look for raw data files
-DATA_PATH = os.environ.get('SILVERLAB_DATA_DIR', '')
 
 
 def do_header231_test(lab_view_header, expected):
@@ -16,19 +11,8 @@ def do_header231_test(lab_view_header, expected):
     assert header.imaging_mode == expected['mode']
 
 
-@pytest.mark.skipif(
-    not os.path.isdir(DATA_PATH),
-    reason="raw data folder '{}' not present".format(DATA_PATH))
 class TestLabView231Header(object):
 
     def test_synthetic_header(self):
         expected = {'version': LabViewVersions.v231, 'number of trials': 2, 'mode': Modes.miniscan}
         do_header231_test(".\\tests\\data\\Experiment Header v231.ini", expected)
-
-    def test_hg_30_exp01(self):
-        expected = {'version': LabViewVersions.v231, 'number of trials': 30, 'mode': Modes.miniscan}
-        do_header231_test(DATA_PATH + "\\LabViewData2020\\HG_30_exp01\\200107_13_13_33 FunctAcq\\Experiment Header.ini", expected)
-
-    def test_hg_29_exp02(self):
-        expected = {'version': LabViewVersions.v231, 'number of trials': 30, 'mode': Modes.miniscan}
-        do_header231_test(DATA_PATH + "\\LabViewData2020\\HG_29_exp02\\200206_16_30_32 FunctAcq\\Experiment Header.ini", expected)
