5cd095831210ed4827078239c0e27463edb0c480
alessandrofelder <a.felder@ucl.ac.uk>
2020-04-23 15:10:18 +0100

    rename fixture

--- tests/test_labview231_header.py
+++ tests/test_labview231_header.py
@@ -9,7 +9,7 @@ expected = {'version': LabViewVersions.v231,
 
 
 @pytest.fixture(params=[os.path.join("tests", "data", "Experiment Header v231.ini")])
-def get_header_from_file(request):
+def header(request):
     """create header object from a LabView header file."""
     header_file = request.param
     header_object = LabViewHeader.from_file(header_file)
@@ -18,13 +18,13 @@ def get_header_from_file(request):
 
 class TestLabView231Header(object):
 
-    def test_lab_view_version(self, get_header_from_file):
-        assert get_header_from_file.version == expected['version']
+    def test_lab_view_version(self, header):
+        assert header.version == expected['version']
 
-    def test_imaging_mode(self, get_header_from_file):
-        assert get_header_from_file.imaging_mode == expected['mode']
+    def test_imaging_mode(self, header):
+        assert header.imaging_mode == expected['mode']
 
-    def test_trial_times(self, get_header_from_file):
-        trial_times = get_header_from_file.determine_trial_times()
+    def test_trial_times(self, header):
+        trial_times = header.determine_trial_times()
         assert len(trial_times) == len(expected['trial times'])
         assert trial_times == expected['trial times']
