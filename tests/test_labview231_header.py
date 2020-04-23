a073c520621a018f0366d06adf389cd0f0134d8c
alessandrofelder <a.felder@ucl.ac.uk>
2020-04-23 15:08:31 +0100

    more thorough trial time testing

--- tests/test_labview231_header.py
+++ tests/test_labview231_header.py
@@ -3,12 +3,14 @@ import pytest
 
 from silverlabnwb.header import LabViewHeader, LabViewVersions, Modes
 
-expected = {'version': LabViewVersions.v231, 'number of trials': 2, 'mode': Modes.miniscan}
+expected = {'version': LabViewVersions.v231,
+            'mode': Modes.miniscan,
+            'trial times': [(0.0, 12.345678), (12.567890, 23.456789)]}
 
 
 @pytest.fixture(params=[os.path.join("tests", "data", "Experiment Header v231.ini")])
 def get_header_from_file(request):
-    """create header object from a Labview header file."""
+    """create header object from a LabView header file."""
     header_file = request.param
     header_object = LabViewHeader.from_file(header_file)
     return header_object
@@ -24,4 +26,5 @@ class TestLabView231Header(object):
 
     def test_trial_times(self, get_header_from_file):
         trial_times = get_header_from_file.determine_trial_times()
-        assert len(trial_times) == expected['number of trials']
+        assert len(trial_times) == len(expected['trial times'])
+        assert trial_times == expected['trial times']
