abd7c7586fb49cedff06b3fee482f2e02afea196
alessandrofelder <a.felder@ucl.ac.uk>
2020-04-23 13:07:16 +0100

    attempt at modularising header tests
    more unit-test-like (one thing per test)
    and possibly a step on the way to me understanding pytest better
    but not sure about levels of abstraction: currently in class, could be
    parameter of fixture (I think!)

--- tests/test_labview231_header.py
+++ tests/test_labview231_header.py
@@ -1,19 +1,33 @@
 import os
+import pytest
 
 from silverlabnwb.header import LabViewHeader, LabViewVersions, Modes
 
 
-def do_header_test(lab_view_header, expected):
-    header = LabViewHeader.from_file(lab_view_header)
-    trial_times = header.determine_trial_times()
-    assert header.version == expected['version']
-    assert len(trial_times) == expected['number of trials']
-    assert header.imaging_mode == expected['mode']
+@pytest.fixture
+def get_header_from_file(request):
+    """create header object from a Labview header file."""
+    header_file = getattr(request.cls, "header_file")
+    header_object = LabViewHeader.from_file(header_file)
+    return header_object
+
+
+@pytest.fixture
+def get_expected(request):
+    """create dictionary of expected values for a Labview header"""
+    return getattr(request.cls, "expected")
 
 
 class TestLabView231Header(object):
+    header_file = os.path.join("tests", "data", "Experiment Header v231.ini")
+    expected = {'version': LabViewVersions.v231, 'number of trials': 2, 'mode': Modes.miniscan}
 
-    def test_synthetic_v231header(self):
-        expected = {'version': LabViewVersions.v231, 'number of trials': 2, 'mode': Modes.miniscan}
-        path_to_header = os.path.join("tests", "data", "Experiment Header v231.ini")
-        do_header_test(path_to_header, expected)
+    def test_lab_view_version(self, get_header_from_file, get_expected):
+        assert get_header_from_file.version == get_expected['version']
+
+    def test_imaging_mode(self, get_header_from_file, get_expected):
+        assert get_header_from_file.imaging_mode == get_expected['mode']
+
+    def test_trial_times(self, get_header_from_file, get_expected):
+        trial_times = get_header_from_file.determine_trial_times()
+        assert len(trial_times) == get_expected['number of trials']
