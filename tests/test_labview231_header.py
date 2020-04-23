aed643bf43635fabd541a839a19401b2705807e3
alessandrofelder <a.felder@ucl.ac.uk>
2020-04-23 14:51:01 +0100

    simplify and generalise design
    not sure how we will generalise for different expected in an elegant way

--- tests/test_labview231_header.py
+++ tests/test_labview231_header.py
@@ -3,31 +3,25 @@ import pytest
 
 from silverlabnwb.header import LabViewHeader, LabViewVersions, Modes
 
+expected = {'version': LabViewVersions.v231, 'number of trials': 2, 'mode': Modes.miniscan}
 
-@pytest.fixture
+
+@pytest.fixture(params=[os.path.join("tests", "data", "Experiment Header v231.ini")])
 def get_header_from_file(request):
     """create header object from a Labview header file."""
-    header_file = getattr(request.cls, "header_file")
+    header_file = request.param
     header_object = LabViewHeader.from_file(header_file)
     return header_object
 
 
-@pytest.fixture
-def get_expected(request):
-    """create dictionary of expected values for a Labview header"""
-    return getattr(request.cls, "expected")
-
-
 class TestLabView231Header(object):
-    header_file = os.path.join("tests", "data", "Experiment Header v231.ini")
-    expected = {'version': LabViewVersions.v231, 'number of trials': 2, 'mode': Modes.miniscan}
 
-    def test_lab_view_version(self, get_header_from_file, get_expected):
-        assert get_header_from_file.version == get_expected['version']
+    def test_lab_view_version(self, get_header_from_file):
+        assert get_header_from_file.version == expected['version']
 
-    def test_imaging_mode(self, get_header_from_file, get_expected):
-        assert get_header_from_file.imaging_mode == get_expected['mode']
+    def test_imaging_mode(self, get_header_from_file):
+        assert get_header_from_file.imaging_mode == expected['mode']
 
-    def test_trial_times(self, get_header_from_file, get_expected):
+    def test_trial_times(self, get_header_from_file):
         trial_times = get_header_from_file.determine_trial_times()
-        assert len(trial_times) == get_expected['number of trials']
+        assert len(trial_times) == expected['number of trials']
