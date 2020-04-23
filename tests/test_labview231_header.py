e53c056bddc150819d8a4c5efd5c6cf3dd2ee7ce
alessandrofelder <a.felder@ucl.ac.uk>
2020-04-23 10:18:22 +0100

    call from_file on base class
    more thorough test: the correct derived header should be found when
    calling from_file on base class.
    functions renamed accordingly.

--- tests/test_labview231_header.py
+++ tests/test_labview231_header.py
@@ -1,10 +1,10 @@
 import os
 
-from silverlabnwb.header import LabViewHeader231, LabViewVersions, Modes
+from silverlabnwb.header import LabViewHeader, LabViewVersions, Modes
 
 
-def do_header231_test(lab_view_header, expected):
-    header = LabViewHeader231.from_file(lab_view_header)
+def do_header_test(lab_view_header, expected):
+    header = LabViewHeader.from_file(lab_view_header)
     trial_times = header.determine_trial_times()
     assert header.version == expected['version']
     assert len(trial_times) == expected['number of trials']
@@ -13,7 +13,7 @@ def do_header231_test(lab_view_header, expected):
 
 class TestLabView231Header(object):
 
-    def test_synthetic_header(self):
+    def test_synthetic_v231header(self):
         expected = {'version': LabViewVersions.v231, 'number of trials': 2, 'mode': Modes.miniscan}
         path_to_header = os.path.join("tests", "data", "Experiment Header v231.ini")
-        do_header231_test(path_to_header, expected)
+        do_header_test(path_to_header, expected)
