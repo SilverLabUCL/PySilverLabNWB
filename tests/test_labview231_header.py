cb946c389ff6cdff4aa1a15eb99373e90b4f954f
alessandrofelder <a.felder@ucl.ac.uk>
2020-04-28 13:31:19 +0100

    add tests for real life header

--- tests/test_labview231_header.py
+++ tests/test_labview231_header.py
@@ -18,17 +18,20 @@ class TestLabViewHeaders(object):
     synthetic_header_path_v231 = 'synthetic experiment Header v231.ini'
     synthetic_header_path_v231_no_last_time = 'synthetic experiment Header v231 no last time.ini'
     synthetic_header_path_pre2018 = 'Experiment Header.ini'
+    real_life_header_path_v231_pointing = 'real life Experiment Header v231 pointing.ini'
 
     @pytest.mark.parametrize("header, expected_version",
                              [(synthetic_header_path_v231, LabViewVersions.v231),
-                              (synthetic_header_path_pre2018, LabViewVersions.pre2018)],
+                              (synthetic_header_path_pre2018, LabViewVersions.pre2018),
+                              (real_life_header_path_v231_pointing, LabViewVersions.v231)],
                              indirect=["header"])
     def test_lab_view_version(self, header, expected_version):
         assert header.version == expected_version
 
     @pytest.mark.parametrize("header, expected_mode",
                              [(synthetic_header_path_v231, Modes.miniscan),
-                              (synthetic_header_path_pre2018, Modes.pointing)],
+                              (synthetic_header_path_pre2018, Modes.pointing),
+                              (real_life_header_path_v231_pointing, Modes.pointing)],
                              indirect=["header"])
     def test_imaging_mode(self, header, expected_mode):
         assert header.imaging_mode == expected_mode
@@ -39,6 +42,12 @@ class TestLabViewHeaders(object):
                              indirect=["header"])
     def test_trial_times(self, header, expected_trial_times):
         assert header.determine_trial_times() == expected_trial_times
+
+    @pytest.mark.parametrize("header, expected_trial_time_length",
+                             [(real_life_header_path_v231_pointing, 29)],
+                             indirect=["header"])
+    def test_trial_times_length(self, header, expected_trial_time_length):
+        assert len(header.determine_trial_times()) == expected_trial_time_length
 
     @pytest.mark.parametrize("header",
                              [synthetic_header_path_pre2018],
