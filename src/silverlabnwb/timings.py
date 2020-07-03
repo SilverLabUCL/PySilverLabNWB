a9344623cd877968ffa493811fceefc88081d371
alessandrofelder <a.felder@ucl.ac.uk>
2020-07-03 15:25:56 +0100

    add cycle time estimation

--- src/silverlabnwb/timings.py
+++ src/silverlabnwb/timings.py
@@ -44,28 +44,43 @@ class LabViewTimings231(LabViewTimings):
         # in those cases, we will need to access each individual row here separately.
         # for now, take first row and all ROIs are same size and orientation
         if roi_data['Angle (deg)'][0] == 0:
-            n_lines_per_roi = int(roi_data['Y stop'][0]-roi_data['Y start'][0])
+            n_lines_per_roi = int(roi_data['Y stop'][0] - roi_data['Y start'][0])
         else:
-            n_lines_per_roi = int(roi_data['X stop'][0]-roi_data['X start'][0])
+            n_lines_per_roi = int(roi_data['X stop'][0] - roi_data['X start'][0])
         self.pixel_time_offsets = np.reshape(self.pixel_time_offsets.values,
                                              (n_trials * n_cycles_per_trial,
                                               n_rois,
                                               n_lines_per_roi))
-        self.cycle_time = 0  # TODO!!!
+
+        # estimate time for one cycle by averaging the time it takes for the first cycle of each trial.
+        # The n_pixels_per_line * pixel_dwell_time contribution of the last line is negligible.
+        first_cycle_times_for_each_trial = []
+        for i in list(range(n_trials)):
+            first_cycle_times_for_each_trial.append(self.pixel_time_offsets
+                                                    [i * n_cycles_per_trial]  # offset previous trials
+                                                    [n_rois - 1]  # last ROI
+                                                    [n_lines_per_roi - 1])  # start of last line
+        print(first_cycle_times_for_each_trial)
+        self.cycle_time = np.mean(first_cycle_times_for_each_trial)  # will this introduce more error than it
+        # avoids?? possibly better to keep everything in us for a while?
 
 
+# visual integration tests until we implement pytests.
+# TODO pytests
+# TODO documentation
+# TODO remove the __main__ function
 if __name__ == "__main__":
     old_file_path = "C:\\Users\\Alessandro\\Documents\\UCL-projects\\silverlab\\Data" \
                     "\\sample_miniscan_fred_170322_14_06_43\\Single cycle relative times.txt "
-    print("old pixel time offsets: ", LabViewTimingsPre2018(old_file_path).pixel_time_offsets)
-    print("new cycle time: ", LabViewTimingsPre2018(old_file_path).cycle_time)
+    old_timings = LabViewTimingsPre2018(old_file_path)
+    print("old pixel time offsets: ", old_timings.pixel_time_offsets)
+    print("old cycle time: ", old_timings.cycle_time)
 
     new_file_path = "C:\\Users\\Alessandro\\Documents\\UCL-projects\\silverlab\\Data\\" \
                     "LabViewData2020\\HG_30_exp01\\200107_13_13_33 FunctAcq\\Single cycle relative times_HW.txt"
     new_roi_path = "C:\\Users\\Alessandro\\Documents\\UCL-projects\\silverlab\\Data\\" \
                    "LabViewData2020\\HG_30_exp01\\200107_13_13_33 FunctAcq\\ROI.dat"
-    print("new pixel time offsets: ",
-          LabViewTimings231(new_file_path, roi_path=new_roi_path, cycles_per_trial=578, n_trials=30).pixel_time_offsets)
-    print("new cycle time: ",
-          LabViewTimings231(new_file_path, roi_path=new_roi_path, cycles_per_trial=578, n_trials=30).cycle_time)
-    print(LabViewTimings231(new_file_path, roi_path=new_roi_path, cycles_per_trial=578, n_trials=30).pixel_time_offsets[0].shape)
+    new_timings = LabViewTimings231(new_file_path, roi_path=new_roi_path, cycles_per_trial=578, n_trials=30)
+    print("new pixel time offsets: ", new_timings.pixel_time_offsets)
+    print("new cycle time: ", new_timings.cycle_time)
+
