37155f413612825f6768559152bf9d8ddc0cfcae
alessandrofelder <a.felder@ucl.ac.uk>
2020-07-08 14:29:39 +0100

    main function 'integration test' and printouts no longer needed

--- src/silverlabnwb/timings.py
+++ src/silverlabnwb/timings.py
@@ -49,7 +49,6 @@ class LabViewTimings231(LabViewTimings):
             n_lines_per_roi = int(roi_data['Y stop'][0] - roi_data['Y start'][0])
         else:
             n_lines_per_roi = int(roi_data['X stop'][0] - roi_data['X start'][0])
-        print("lines per ROI", n_lines_per_roi)
         self.pixel_time_offsets = np.reshape(self.pixel_time_offsets.values,
                                              (n_trials * n_cycles_per_trial,
                                               n_rois,
@@ -63,28 +62,7 @@ class LabViewTimings231(LabViewTimings):
                                                     [i * n_cycles_per_trial]  # offset previous trials
                                                     [n_rois - 1]  # last ROI
                                                     [n_lines_per_roi - 1])  # start of last line
-        print(first_cycle_times_for_each_trial)
         self.cycle_time = np.mean(first_cycle_times_for_each_trial)  # will this introduce more error than it
         # avoids?? possibly better to keep everything in us for a while?
         # similarly, we might be better off dividing by 1e6 way later than at read time to avoid numerical error?
-
-
-# visual integration tests until we implement pytests.
-# TODO pytests
-# TODO documentation
-# TODO remove the __main__ function
-if __name__ == "__main__":
-    old_file_path = "C:\\Users\\Alessandro\\Documents\\UCL-projects\\silverlab\\Data" \
-                    "\\sample_miniscan_fred_170322_14_06_43\\Single cycle relative times.txt "
-    old_timings = LabViewTimingsPre2018(old_file_path)
-    print("old pixel time offsets: ", old_timings.pixel_time_offsets)
-    print("old cycle time: ", old_timings.cycle_time)
-
-    new_file_path = "C:\\Users\\Alessandro\\Documents\\UCL-projects\\silverlab\\Data\\" \
-                    "LabViewData2020\\HG_30_exp01\\200107_13_13_33 FunctAcq\\Single cycle relative times_HW.txt"
-    new_roi_path = "C:\\Users\\Alessandro\\Documents\\UCL-projects\\silverlab\\Data\\" \
-                   "LabViewData2020\\HG_30_exp01\\200107_13_13_33 FunctAcq\\ROI.dat"
-    new_timings = LabViewTimings231(new_file_path, roi_path=new_roi_path, cycles_per_trial=578, n_trials=30)
-    print("new pixel time offsets: ", new_timings.pixel_time_offsets)
-    print("new cycle time: ", new_timings.cycle_time)
 
