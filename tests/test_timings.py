c2bfd59c67e1e160faddbd48c1859634ef2a39a8
alessandrofelder <a.felder@ucl.ac.uk>
2020-07-24 09:53:08 +0100

    add tests for timings pre2018

--- tests/test_timings.py
+++ tests/test_timings.py
@@ -15,11 +15,22 @@ def synthetic_timings_v231(ref_data_dir):
     # 2 trials, each of which has 3 cycles and 4 rois a 5 lines
     # and a few zero lines as may be expected "in the wild"
     # first cycle of trial 1 and trial 2 take 1300.4 and 1200.4 nanoseconds, respectively
-    return timings.LabViewTimings231(timings_file_path,
+    return timings.LabViewTimings231(relative_times_path=timings_file_path,
                                      roi_path=roi_file_path,
                                      n_cycles_per_trial=3,
                                      n_trials=2,
                                      dwell_time=1.e-6)
+
+
+@pytest.fixture(scope="module")
+def synthetic_timings_pre2018(ref_data_dir):
+    timings_file_name = "synthetic pre2018 Single cycle relative times.txt"
+    timings_file_path = os.path.join(ref_data_dir, timings_file_name)
+    roi_file_name = "synthetic pre2018 ROI.dat"
+    roi_file_path = os.path.join(ref_data_dir, roi_file_name)
+    return timings.LabViewTimingsPre2018(relative_times_path=timings_file_path,
+                                         roi_path=roi_file_path,
+                                         dwell_time=1.e-6)
 
 
 def test_cycle_time_v231(synthetic_timings_v231):
@@ -27,13 +38,28 @@ def test_cycle_time_v231(synthetic_timings_v231):
     assert synthetic_timings_v231.cycle_time == expected_mean_first_cycle_time_s
 
 
-def test_pixel_time_offsets_for_roi(synthetic_timings_v231):
+def test_pixel_time_offsets_for_roi_v231(synthetic_timings_v231):
     roi_0_offsets = synthetic_timings_v231.pixel_time_offsets[0]
     expected_shape = (6, 5, 6)  # 2 trials * 3 cycles , 5 lines/roi, 6 pixels/line
-    expected_first_cycle_first_row_offset = 200/1e6
-    expected_first_cycle_last_row_offset = 400.4/1e6
-    expected_last_cycle_first_row_offset = 4100/1e6
+    expected_first_cycle_first_row_offset = 200 / 1e6
+    expected_first_cycle_last_row_offset = 400.4 / 1e6
+    expected_last_cycle_first_row_offset = 4100 / 1e6
     assert roi_0_offsets.shape == expected_shape
     assert roi_0_offsets[0][0][0] == expected_first_cycle_first_row_offset
     assert roi_0_offsets[0][4][0] == expected_first_cycle_last_row_offset
     assert roi_0_offsets[5][0][0] == expected_last_cycle_first_row_offset
+
+
+def test_cycle_time_pre2018(synthetic_timings_pre2018):
+    expected_cycle_time = 12345/1e6
+    assert synthetic_timings_pre2018.cycle_time == expected_cycle_time
+
+
+def test_pixel_time_offsets_for_roi_pre2018(synthetic_timings_pre2018):
+    roi_0_offsets = synthetic_timings_pre2018.pixel_time_offsets[0]
+    expected_shape = (4, 10)  # 4 lines/roi, 10 pixels/line
+    expected_first_row_offset = 400 / 1e6
+    expected_last_row_offset = 700 / 1e6
+    assert roi_0_offsets.shape == expected_shape
+    assert roi_0_offsets[0][0] == expected_first_row_offset
+    assert roi_0_offsets[3][0] == expected_last_row_offset
